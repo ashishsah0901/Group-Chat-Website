e5d692baa36b13ce942c6af6318f4c24044cf508
Ashish Sah <ashishsah173@gmail.com>
2021-04-04 11:45:11 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const socket = io('http://localhost:8000');
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container');
+var audio = new Audio('notification.mp3');
+
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if (position == 'left') {
+        audio.play();
+    }
+}
+
+// const userName = prompt("Enter your name to join...");
+socket.emit('new-user-joined', userName);
+
+socket.on('user-joined', data => {
+    append(`${data} joined the chat`, 'left');
+})
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+})
+
+socket.on('leave', name => {
+    append(`${name} left the chat`, 'left');
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You : ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = "";
+})
